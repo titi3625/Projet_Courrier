f714f9c45495af4f1b2805a317864ad8a6edde60
titi3625 <campos.thibault@gmail.com>
2013-02-12 09:54:54 +0100

    premier commit

---
+++ include/menu.php
@@ -0,0 +1,15 @@
+<div class="menu">
+	<div class="separateur"></div>
+	<dl>
+		<dt><a href="index.php">Accueil</a></dt>
+	</dl>
+	<div class="separateur"></div>
+	<dl>
+		<dt><a href="recherche.php">Rechercher</a></dt>
+	</dl>
+	<div class="separateur"></div>
+	<dl>
+		<dt><a href="inserer.php">Inserer</a></dt>
+	</dl>
+	<div class="separateur"></div>
+</div>
---
+++ inserer.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Insertion</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div class="header">
+		<?php
+		include_once('include/menu.php');
+		?>
+	</div>
+	
+	<div class="content">
+		bonjour
+	</div>
+	
+	<div class="footer">
+		
+	</div>
+</body>
+</html>
