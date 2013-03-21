8539af39706a3ee8dcc4bad83d795d124f4a7498
cocolastico <cocolastico@cocolastico-ubuntu.(none)>
2013-03-21 14:03:23 +0100

    virage d'html2pdf

--- include/backupRestore.php
+++
@@ -1,9 +0,0 @@
-<legend>Sauvegarder et restauration de la base de donnée</legend>
-
-
-<form action="sav_bdd.php">
-	<label for="buttonSav">Télécharger la BDD au format SQL : </label>
-	<input type="submit" id="buttonSav" name="buttonSav" value="Telecharger">
-</form>
-
-
